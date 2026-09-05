ee05afa1d3a1352fbeb713445c3af02d2a033974
mohammedelhagi88 <mohammedelhagi88@gmail.com>
2026-09-05 20:10:36 +0200

    Export trading data and service APIs

--- modules/trading/__init__.py
+++ modules/trading/__init__.py
@@ -1,13 +1,18 @@
-"""Trading analysis module."""
+"""Trading analysis and safety-gated orchestration module."""
 
+from .approval import ApprovedTrade, TradingApprovalBridge
 from .config import TradingConfig
 from .engine import MarketSnapshot, TradingEngine
 from .indicators import atr, bollinger_bands, ema, obv, rsi, sma, vwap, wma
+from .market_data import MarketBar, MarketDataProvider, MarketSnapshotRequest, validate_bars
 from .selector import TradeCandidate, TradeSelector, build_levels
+from .service import TradeOpportunity, TradingService
 from .signals import SignalResult, composite_signal
 
 __all__ = [
     "TradingConfig", "MarketSnapshot", "TradingEngine", "TradeCandidate",
     "TradeSelector", "SignalResult", "build_levels", "composite_signal",
+    "TradingApprovalBridge", "ApprovedTrade", "TradingService", "TradeOpportunity",
+    "MarketBar", "MarketDataProvider", "MarketSnapshotRequest", "validate_bars",
     "atr", "bollinger_bands", "ema", "obv", "rsi", "sma", "vwap", "wma",
 ]
