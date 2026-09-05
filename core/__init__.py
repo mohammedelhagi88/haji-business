4a608a91bcd8f78380e008e01bc41f608bf37109
mohammedelhagi88 <mohammedelhagi88@gmail.com>
2026-09-05 19:50:58 +0200

    Export Haji agent and persistent memory

--- core/__init__.py
+++ core/__init__.py
@@ -1,19 +1,26 @@
 """Haji Business core package."""
 
+from .agent import HajiAgent
 from .commands import Command, CommandEngine
 from .memory import MemoryStore
 from .models import ApprovalRequest, MemoryItem, RiskLevel, Task
 from .permissions import PermissionGate
+from .persistent_memory import PersistentMemoryStore
+from .runtime import HajiRuntime, RuntimeEvent
 from .tasks import TaskManager
 
 __all__ = [
     "ApprovalRequest",
     "Command",
     "CommandEngine",
+    "HajiAgent",
+    "HajiRuntime",
     "MemoryItem",
     "MemoryStore",
+    "PersistentMemoryStore",
     "PermissionGate",
     "RiskLevel",
+    "RuntimeEvent",
     "Task",
     "TaskManager",
 ]
