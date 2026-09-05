f43553b6dcee2b002fe4948a7bdcffc1314d595b
mohammedelhagi88 <mohammedelhagi88@gmail.com>
2026-09-05 19:26:09 +0200

    Export trading engine and analysis APIs

--- modules/trading/__init__.py
+++ modules/trading/__init__.py
@@ -1,5 +1,13 @@
 """Trading analysis module."""
 
+from .config import TradingConfig
+from .engine import MarketSnapshot, TradingEngine
 from .indicators import atr, bollinger_bands, ema, obv, rsi, sma, vwap, wma
+from .selector import TradeCandidate, TradeSelector, build_levels
+from .signals import SignalResult, composite_signal
 
-__all__ = ["atr", "bollinger_bands", "ema", "obv", "rsi", "sma", "vwap", "wma"]
+__all__ = [
+    "TradingConfig", "MarketSnapshot", "TradingEngine", "TradeCandidate",
+    "TradeSelector", "SignalResult", "build_levels", "composite_signal",
+    "atr", "bollinger_bands", "ema", "obv", "rsi", "sma", "vwap", "wma",
+]
